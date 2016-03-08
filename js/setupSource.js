8809f01479c5681269a0ddee55b74dd46cb85d2e
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-03-08 10:36:54 -0800

    issue noted with attack and release - still configuring metronome

--- js/setupSource.js
+++ js/setupSource.js
@@ -1,46 +1,70 @@
-var context = new AudioContext();
-var out = context.destination;
-var sources = [];
-var vcos = [];
-var vcas = [];
-var i;
+$(document).ready(function() {
+  var context = new AudioContext();
+  var out = context.destination;
+  var sources = [];
+  var vcos = [];
+  var vcas = [];
+  var i;
+  var now;
+  var beat;
 
-function Source(vco, vcoFreq, vca, vcaGainVal) {
-  this.vco = vco;
-  this.vcoFreq = vcoFreq;
-  this.vca = vca;
-  this.vcaGainVal = vcaGainVal;
-  this.vcaAttack = function() {
-    var now = context.currentTime;
-    this.vcaGainVal.cancelScheduledValues(now);
-    this.vcaGainVal.setValueAtTime(vcaGainVal, now);
-    this.vcaGainVal.linearRampToValueAtTime(1, now + 0.2);
+  // function metronome() {
+  //   now = context.currentTime;
+  //   beat = (1000*60)/($("#bpm").val());
+  //   if((now % beat) === 0) {
+  //     $('.step').toggleClass('trigger');
+  //   }
+  // }
+
+  function Source(vco, vcoType, vcoFreq, vca, vcaGainVal) {
+    this.vco = vco;
+    this.vcoType = vcoType;
+    this.vcoFreq = vcoFreq;
+    this.vca = vca;
+    this.vcaGainVal = vcaGainVal;
+    this.vcaAttack = function () {
+      now = context.currentTime;
+      console.log(now);
+      this.vcaGainVal.cancelScheduledValues(now);
+      this.vcaGainVal.setValueAtTime(this.vcaGainVal, now);
+      this.vcaGainVal.linearRampToValueAtTime(1, now + 0.2);
+      }
+    this.vcaRelease = function() {
+      now = context.currentTime;
+      vca.gain.cancelScheduledValues(now);
+      vca.gain.setValueAtTime(this.vca.gain.value, now);
+      vca.gain.linearRampToValueAtTime(0, now + 3);
+      }
     }
-  this.vcaRelease = function() {
-    var now = context.currentTime;
-    vca.gain.cancelScheduledValues(now);
-    vca.gain.setValueAtTime(vca.gain.value, now);
-    vca.gain.linearRampToValueAtTime(0, now + 3);
+
+  function createSources() {
+    for(i = 0; i < 16; i++) {
+      vcos[i] = context.createOscillator();
+      vcos[i].type = 'sine';
+      vcas[i] = context.createGain();
+
+      vcos[i].connect(vcas[i]);
+      vcas[i].connect(out);
+
+      vcos[i].frequency = 440;
+      vcas[i].gain.value = 0.00;
+
+      sources[i] = new Source(vcos[i], vcos[i].type, vcos[i].frequency, vcas[i], vcas[i].gain.value);
+
+      sources[i].vco.start();
+      }
     }
 
-function createSources() {
-  for(i = 0; i < 16; i++) {
-    vco[i] = context.createOscillator();
-    vca[i] = context.createGain();
+  // metronome();
+  createSources();
 
-    vco[i].connect(vca[i]);
-    vca[i].connect(out);
-    vco[i].start(0);
 
-    vco[i].frequency = 440;
-    vca[i].gain.value = 0;
-
-    sources[i] = new Source(vco[i], vco[i].frequency, vca[i], vca[i].gain.value);
-    }
+  for(i = 0; i < 15; i++) {
+    var $step = $('#step' + [i].toString());
+    $step.click(function() {
+      sources[i].vcaAttack();
+      sources[i].vcaRelease();
+    });
   }
 
-createSources();
-
-for(i = 0; i < 15; i++) {
-  $('#step' + [i].toString()).click(sources[i].vcaAttack(), sources[i].vcaRelease());
-}
+});
